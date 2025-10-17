ee9ab750d55bb0887940e9ff7178be3c5fe2228e
mariaawadd <161076192+mariaawadd@users.noreply.github.com>
2025-10-18 01:34:54 +0300

    frontend

---
+++ smart_hair_advisor/frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./index.css";
+import Chatbot from "./components/Chatbot";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center", marginTop: "20px" }}>Smart Hair Advisor 💬</h1>
+      <Chatbot />
+    </div>
+  );
+}
+
+export default App;
